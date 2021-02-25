04b562ef967989d96f2957c905a617eadea9d21b
Victor Ordu <victorordu@outlook.com>
2021-02-25 16:58:28 +0100

    Initial commit

---
+++ R/run.R
@@ -0,0 +1,25 @@
+#' Run RQDA
+#'
+#' Start up RQDA in a given session.
+#'
+#' @import RSQLite
+#'
+#' @note Also attaches RSQLite package to the search path
+#'
+#' @export
+start <- function() {
+  .f <- function(p) {
+    if (!requireNamespace(p, quietly = TRUE)) {
+      msg <- paste(p, "is not installed")
+      if (p == "RQDA")
+        msg <- sprintf("%s. Install it with RQDAassist::install()")
+      stop(msg, call. = FALSE)
+    }
+    if (p %in% .packages())
+      return()
+    attachNamespace(p)
+  }
+
+  pkgs <- c("RSQLite", "RQDA")
+  invisible(lapply(pkgs, .f))
+}

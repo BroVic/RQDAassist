e60b2bbc8a22ed0c281868518423d854a0da7b33
Victor Ordu <victorordu@outlook.com>
2021-02-26 13:37:10 +0100

    Add argument to search function
    The function can now, optionally, return a list of the matching codes instead of a data frame. This will be the default case.

--- R/search.R
+++ R/search.R
@@ -2,17 +2,23 @@ globalVariables("codename")
 #' Search for codes using a given term
 #'
 #' @param searchterm A string.
+#' @param codes.only Logical; whether to fetch only the codes or a table with
+#' other related variables. Defaults to \code{TRUE}.
 #'
 #' @return An object of class \code{data.frame}, which is subset of the coding
-#' table.
+#' table, or in the case of \code{codes.only == TRUE}, a list of matching
+#' codes.
 #'
 #' @export
-search_codes <- function(searchterm) {
+search_codes <- function(searchterm, codes.only = TRUE) {
   if (!is.character(searchterm) && length(searchterm) != 1L)
     stop("Expected a string as argument")
   if (!nchar(searchterm))
     stop("Search cannot be conducted with an empty string")
   start()
   cdt <- eval(call("getCodingTable"))
-  subset(cdt, grepl(searchterm, codename, ignore.case = TRUE))
+  sub <- subset(cdt, grepl(searchterm, codename, ignore.case = TRUE))
+  if (codes.only)
+    return(unique(sub$codename))
+  sub
 }

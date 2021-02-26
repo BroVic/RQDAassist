726a27ddd134d6d52ae8ada2f259c815bd5228b3
Victor Ordu <victorordu@outlook.com>
2021-02-26 13:30:40 +0100

    Add functionality for writing files to projects

---
+++ tests/testthat/test-write.R
@@ -0,0 +1,22 @@
+library(stringi)
+
+
+test_that("multiplication works", {
+  dir <- "data"
+  fname1 <- "lipsum2.txt"
+  fname2 <- "lipsum3.txt"
+  file1 <- file.path(dir, fname1)
+  file2 <- file.path(dir, fname2)
+  cat(stri_rand_lipsum(2), file = file1)
+  cat(stri_rand_lipsum(3), file = file2)
+  files <- c(file1, file2)
+
+  out <- make_FileList(files)
+
+  expect_type(out, "list")
+  expect_is(out, "list")
+  expect_named(out, c(fname1, fname2))
+  expect_length(out, 2L)
+
+  file.remove(files)
+})

5152671e31f9a4278021617bb2926cd406e69814
Victor Ordu <victorordu@outlook.com>
2021-03-05 15:37:42 +0100

    Fix broken unit test

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -1,21 +1,20 @@
 library(stringi)
 
-
 test_that("FileList is created", {
   dir <- "data"
   fname1 <- "lipsum2.txt"
   fname2 <- "lipsum3.txt"
   file1 <- file.path(dir, fname1)
   file2 <- file.path(dir, fname2)
-  cat(stri_rand_lipsum(2), file = file1)
-  cat(stri_rand_lipsum(3), file = file2)
+  cat(stri_rand_lipsum(2), file = file1, fill = TRUE)
+  cat(stri_rand_lipsum(3), file = file2, fill = TRUE)
   files <- c(file1, file2)
 
   out <- make_FileList(files)
 
   expect_type(out, "list")
   expect_is(out, "list")
-  expect_named(out, c(fname1, fname2))
+  expect_named(out, sub("\\.txt", "", basename(files)))
   expect_length(out, 2L)
 
   file.remove(files)

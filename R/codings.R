93932608230100a3ea985c954343104e68295516
Victor Ordu <victorordu@outlook.com>
2021-03-14 20:29:16 +0100

    Add sections to help file

--- R/codings.R
+++ R/codings.R
@@ -4,15 +4,22 @@ globalVariables(c("cid", "codecat"))
 #' Fetches a table that has the codings as well as other details
 #' related to codings as stored in the database.
 #'
-#' @param proj Path to an RQDA project.
-#' @param query A string; an SQL query. The default query selects
-#' the following columns from the RQDA file: \code{cid}  from the
-#' \code{treecode} table and \code{name} from the \code{codecat}
-#' table. (For details on the database refer to )
+#' @param proj Path to an RQDA project i.e. must have the file extension
+#' \emph{.rqda}.
+#' @param query A string; an SQL query.
+#'
+#' @details The default query selects the following columns from the RQDA
+#' file: \code{cid}  from the \code{treecode} table and \code{name} from
+#' the \code{codecat} table. (For details on the database refer to
+#' \code{\link[RQDA]{RQDATables}})
 #'
 #' @importFrom dplyr `%>%`
 #' @importFrom dplyr count
 #' @importFrom dplyr group_by
+#'
+#' @return A data frame containing details about codings, including the
+#' code name, filenames, starting and ending index, and code categories (if
+#' applicable).
 #'
 #' @export
 retrieve_codingtable <- function(proj, query = NULL) {

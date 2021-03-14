d9ee123a4060c42025641f43df994bb031c97be0
Victor Ordu <victorordu@outlook.com>
2021-03-14 18:49:26 +0100

    Add input validation when reading transcripts

--- R/read.R
+++ R/read.R
@@ -13,29 +13,30 @@
 #'
 #' @export
 read_transcript <- function(destdir, docxfiles) {
+  if (is.null(destdir) || missing(destdir))
+    destdir <- getwd()
+
+  tryCatch({
+    destdir <- normalizePath(destdir)
+  },
+  warning = function(warn) {
+    stop(sprintf("Directory '%s' does not exist", basename(destdir)))
+  })
+
+  # TODO: Add '...' to control this function from without?
   docdt <- readtext::readtext(docxfiles)
-
-  make_safe_names <- function(str, to.lower = FALSE) {
-    # stopifnot(is.character(str))
-    str <- str %>%
-      str_trim() %>%
-      str_squish() %>%
-      str_replace_all("\\s|\\?|\\!|\\*|\\^|&", '-') %>%
-      str_replace_all('-{2,}', '-')
-
-    if (to.lower)
-      str <- str_to_lower(str)
-    str
-  }
 
   docdt <- docdt %>%
     mutate(txt_id = str_replace(doc_id, '(.+)(\\.docx?$)', '\\1.txt')) %>%
-    mutate(txt_id = make_safe_names(txt_id))
+    mutate(txt_id = .makeSafeNames(txt_id))
 
+  ## This function needs to be defined here so that
+  ## it can find 'destdir'. Its purpose is to create
+  ## the eventual filepath of the text file and return it.
   .createFileAndReturnPath <- function(txt, fname) {
     fpath <- file.path(destdir, fname)
     cat(txt, file = fpath)
-    normalizePath(fpath)
+    normalizePath(fpath, winslash = "/")
   }
 
   docdt$text %>%
@@ -44,7 +45,18 @@ read_transcript <- function(destdir, docxfiles) {
 
 
 
+ .makeSafeNames <- function(str, to.lower = FALSE) {
+    # stopifnot(is.character(str))
+    str <- str %>%
+      str_trim %>%
+      str_squish %>%
+      str_replace_all("\\s|\\?|\\!|\\*|\\^|&", '-') %>%
+      str_replace_all('-{2,}', '-')
 
+    if (to.lower)
+      str <- str_to_lower(str)
+    str
+  }
 
 
 
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,6 +1,22 @@
+# test-read.R
+
+test_that("validate input for 'read_transcript()'", {
+  tmpdir <- tempdir()
+  errNoFiles <- "argument \"docxfiles\" is missing, with no default"
+
+  expect_error(read_transcript(NULL), errNoFiles)
+  expect_error(read_transcript(tmpdir), errNoFiles)
+  expect_error(read_transcript(tmpdir, "filenoexist.docx"),
+               "File '' does not exist.")
+  expect_error(read_transcript('fakedir'),
+               "Directory 'fakedir' does not exist")
+})
+
+
+
 test_that("filepaths are returned", {
   fp <- read_transcript("data", "data/lorem.docx")
-  onetxt <- normalizePath("data/lorem.txt")
+  onetxt <- normalizePath("data/lorem.txt", winslash = "/")
 
   expect_equal(fp, onetxt)
 
